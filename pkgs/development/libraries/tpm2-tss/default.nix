63377d45e864f3c0223f6294df46329b994cad0c
Konstantin Bogdanov <var1able@var1able.ru>
2024-12-13 18:54:19 +0100

    Skip tests in tpm2-tss

--- pkgs/development/libraries/tpm2-tss/default.nix
+++ pkgs/development/libraries/tpm2-tss/default.nix
@@ -138,7 +138,7 @@ stdenv.mkDerivation rec {
   '';
 
   doCheck = false;
-  doInstallCheck = stdenv.buildPlatform.canExecute stdenv.hostPlatform;
+  doInstallCheck = false;
   # Since we rewrote the load path in the dynamic loader for the TCTI
   # The various tcti implementation should be placed in their target directory
   # before we could run tests, so we make turn checkPhase into installCheckPhase
