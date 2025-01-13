45a394e437ec2c887d54db976558b4a1cb21af68
Konstantin Bogdanov <var1able@var1able.ru>
2025-01-13 09:13:56 +0100

    cherrypick from upstream: bpftools: fix cross for riscv64-linux

--- pkgs/by-name/bp/bpftools/package.nix
+++ pkgs/by-name/bp/bpftools/package.nix
@@ -1,6 +1,7 @@
 {
   lib,
   stdenv,
+  fetchpatch,
   linuxHeaders,
   libopcodes,
   libopcodes_2_38,
@@ -24,6 +25,25 @@ stdenv.mkDerivation rec {
   patches = [
     # fix unknown type name '__vector128' on ppc64le
     ./include-asm-types-for-ppc64le.patch
+
+    (fetchpatch {
+      # libbpf: Add missing per-arch include path
+      # https://patchwork.kernel.org/project/linux-riscv/patch/20240927131355.350918-1-bjorn@kernel.org/
+      url = "https://patchwork.kernel.org/project/linux-riscv/patch/20240927131355.350918-1-bjorn@kernel.org/raw/";
+      hash = "sha256-edXY/ejHW5L7rGgY5B2GmVZxUgnLdBadNhBOSAgcL7M=";
+    })
+    (fetchpatch {
+      # selftests: bpf: Add missing per-arch include path
+      # https://patchwork.kernel.org/project/linux-riscv/patch/20240927131355.350918-2-bjorn@kernel.org/
+      url = "https://patchwork.kernel.org/project/linux-riscv/patch/20240927131355.350918-2-bjorn@kernel.org/raw/";
+      hash = "sha256-7yNWE/L/qd3vcLtJYoSyGxB3ytySlr20R0D3t5ni2Fc=";
+    })
+    (fetchpatch {
+      # tools: Override makefile ARCH variable if defined, but empty
+      # https://patchwork.kernel.org/project/linux-riscv/patch/20241127101748.165693-1-bjorn@kernel.org/
+      url = "https://patchwork.kernel.org/project/linux-riscv/patch/20241127101748.165693-1-bjorn@kernel.org/raw/";
+      hash = "sha256-y8N71Hm1XfX9g3S6PzW2m7Lxp6wQQMfQE9L0QNt8cYY=";
+    })
   ];
 
   nativeBuildInputs = [
@@ -65,6 +85,9 @@ stdenv.mkDerivation rec {
     "bpf_asm"
     "bpf_dbg"
   ];
+
+  # needed for cross to riscv64
+  makeFlags = [ "ARCH=${stdenv.hostPlatform.linuxArch}" ];
 
   installPhase = ''
     make -C bpftool install
